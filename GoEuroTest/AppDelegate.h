ef8e0980dc85d33d46869f60c11cb2629d61e0e4
Peter Prokop <pitrowech@gmail.com>
2017-02-10 15:01:05 +0100

    Initial Commit

---
+++ GoEuroTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoEuroTest
+//
+//  Created by Peter Prokop on 10/02/2017.
+//  Copyright © 2017 Prokop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GoEuroTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GoEuroTest
+//
+//  Created by Peter Prokop on 10/02/2017.
+//  Copyright © 2017 Prokop. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
